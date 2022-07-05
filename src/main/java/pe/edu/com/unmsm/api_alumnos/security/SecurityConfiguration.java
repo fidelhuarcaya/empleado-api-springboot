e152e8ff97c453ba0d7ad01c32887441f5ae872e
Fidel Huarcaya <fidelhuarcaya20@gmail.com>
2022-07-05 14:01:00 -0500

    pom.xml update

--- src/main/java/pe/edu/com/unmsm/api_alumnos/security/SecurityConfiguration.java
+++ src/main/java/pe/edu/com/unmsm/api_alumnos/security/SecurityConfiguration.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
