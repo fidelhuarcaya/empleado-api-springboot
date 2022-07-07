81dfdcf05f64be46f7b17702318e4d87bb63bbef
Fidel Huarcaya <fidelhuarcaya@gmail.com>
2022-07-06 22:39:07 -0500

    Changes

--- src/main/java/pe/edu/com/unmsm/api_alumnos/controller/ControllerEmpleado.java
+++ src/main/java/pe/edu/com/unmsm/api_alumnos/controller/ControllerEmpleado.java
@@ -9,13 +9,13 @@ import pe.edu.com.unmsm.api_alumnos.service.ServiceAlumnoImpl;
 
 
 @RestController
-@RequestMapping("/api/v1/alumnos")
-public class ControllerAlumno {
+@RequestMapping("/api/v1/empleados")
+public class ControllerEmpleado {
 
     private final ServiceAlumnoImpl serviceAlumno;
 
     @Autowired
-    public ControllerAlumno(ServiceAlumnoImpl serviceAlumno) {
+    public ControllerEmpleado(ServiceAlumnoImpl serviceAlumno) {
         this.serviceAlumno = serviceAlumno;
     }
 
@@ -24,12 +24,12 @@ public class ControllerAlumno {
         return new ResponseEntity<>(serviceAlumno.getAllAlumnos(), HttpStatus.OK);
     }
     @PostMapping
-    private Trabajador addAlumno(@RequestBody Trabajador trabajador){
+    private Trabajador addEmpleado(@RequestBody Trabajador trabajador){
         serviceAlumno.insertAlumno(trabajador);
         return serviceAlumno.insertAlumno(trabajador);
     }
     @PutMapping
-    public ResponseEntity<Object> updateAlumno(@RequestBody Trabajador trabajador) {
+    public ResponseEntity<Object> updateEmpleado(@RequestBody Trabajador trabajador) {
         System.out.println(trabajador.toString());
         if (serviceAlumno.getAlumno(trabajador.getId()).isPresent())
         serviceAlumno.updateAlumno(trabajador);
@@ -37,12 +37,12 @@ public class ControllerAlumno {
         return new ResponseEntity<>("Alumno is updated successsfully", HttpStatus.OK);
     }
     @DeleteMapping("/{id}")
-    private ResponseEntity<Object> deleteAlumno(@PathVariable("id") Long id){
+    private ResponseEntity<Object> deleteEmpleado(@PathVariable("id") Long id){
         serviceAlumno.deleteAlumno(id);
         return new ResponseEntity<>("Alumno is deleted successsfully", HttpStatus.OK);
     }
     @GetMapping("/{id}")
-    private ResponseEntity<Object> getAlumno(@PathVariable("id") Long id){
+    private ResponseEntity<Object> getEmpleado(@PathVariable("id") Long id){
         return new ResponseEntity<>(serviceAlumno.getAlumno(id), HttpStatus.OK);
     }
 }
